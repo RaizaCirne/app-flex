976ad12d64eef195b3f141068da73627405ebd6b
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-11-17 12:07:35 -0300

    missao pratica 2 do primeiro semestre

---
+++ 1º Semestre/Missões Práticas/Nível 2/missao-pratica-2.py
@@ -0,0 +1,5 @@
+"""Implementar um algoritmo de conversão de um número da base decimal para base binária
+
+1º Objetivo: Utilizar recursos lógicos da linguaguem de programação Python, com comandos iterativos.
+2º Objetivo: Entrar com qualquer número inteiro positivo na base decimal e o programa deve produzir o número
+correspondente na base binária."""
