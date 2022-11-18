2ce80e66d0948dc81e17b1b17c8b12bd3e1b4f9e
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-11-18 12:01:18 -0300

    descricao missao pratica nivel 3 do primeiro semestre

---
+++ 1º Semestre/Missões Práticas/Nível 3/missao-pratica-3.py
@@ -0,0 +1,7 @@
+"""
+Implementar um algoritmo de para obter o conjunto das partes de um conjunto
+
+1º Objetivo: Utilizar estruturas de dados como listas, comandos condicionais e iterativos.
+2º Objetivo: Entrar com uma lista que representa um conjunto e o programa deve produzir o seu respectivo conjunto
+das partes.
+"""
