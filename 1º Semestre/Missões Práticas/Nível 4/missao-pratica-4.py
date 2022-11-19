89e8032ad07957581c32dfc1eabdcd26258e44f7
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-11-19 17:10:49 -0300

    descricao missao pratica nivel 4 primeiro semestre

---
+++ 1º Semestre/Missões Práticas/Nível 4/missao-pratica-4.py
@@ -0,0 +1,9 @@
+"""
+Implementar um programa orientado a objetos para visualizar e aplicar regressão linear em séries temporais
+
+1º Objetivo: Aplicar as técnicas de: programação orientada a objetos, manipulação de dicionários e comandos iterativos
+para produção de gráficos no Python e para obter a regressão linear de uma série temporal.
+
+2º Objetivo: Entrar com listas de despesas por dia e o programa deve produzir: gráficos com visualização das séries
+ temporais de despesas; gráfico com a regressão linear da série temporal.
+ """
