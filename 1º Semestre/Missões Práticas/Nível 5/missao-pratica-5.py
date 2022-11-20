cf843c244c94d627f10db978c5f7cda245e0e33b
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-11-20 10:14:09 -0300

    descricao missao pratica nivel 5 primeiro semestre

---
+++ 1º Semestre/Missões Práticas/Nível 5/missao-pratica-5.py
@@ -0,0 +1,8 @@
+"""
+Implementar um programa para manipular dados de um arquivo texto e visualizá-los em um histograma e nuvem de palavras.
+
+1º Objetivo: Gerar dados de teste.
+2º Objetivo: Gravar e recuperar dados de arquivos textos.
+3º Objetivo: Manipular os dados para visualizá-los em um histograma.
+4º Objetivo: Manipular os dados para visualizá-los em uma nuvem de palavras.
+"""
