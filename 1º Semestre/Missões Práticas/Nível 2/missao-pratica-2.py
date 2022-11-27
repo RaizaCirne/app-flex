e735c046f18cc790c263d824271d9cd5e4631d4d
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-11-27 17:31:16 -0300

    add missao pratica nv2

--- 1º Semestre/Missões Práticas/Nível 2/missao-pratica-2.py
+++ 1º Semestre/Missões Práticas/Nível 2/missao-pratica-2.py
@@ -5,3 +5,17 @@ Implementar um algoritmo de conversão de um número da base decimal para base b
 2º Objetivo: Entrar com qualquer número inteiro positivo na base decimal e o programa deve produzir o número
 correspondente na base binária.
 """
+
+num = eval(input('Informe o número inteiro desejado: '))
+n = num
+resultadodiv = 1
+lista = []
+
+while resultadodiv >= 1:
+    resto = num % 2
+    lista.insert(0, resto)
+    resultadodiv = num // 2
+    num = resultadodiv
+
+binario = ''.join([str(item) for item in lista])
+print('O número {} vale {} na base binária'.format(n, binario))
