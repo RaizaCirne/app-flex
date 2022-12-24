eb05ffe76b16be77144fb644870dc0672ed29816
Raíza Cirne Braz <raizabraz93@gmail.com>
2022-12-23 22:28:07 -0300

    add missao pratica nv3

--- 1º Semestre/Missões Práticas/Nível 3/missao-pratica-3.py
+++ 1º Semestre/Missões Práticas/Nível 3/missao-pratica-3.py
@@ -5,3 +5,25 @@ Implementar um algoritmo de para obter o conjunto das partes de um conjunto
 2º Objetivo: Entrar com uma lista que representa um conjunto e o programa deve produzir o seu respectivo conjunto
 das partes.
 """
+
+
+# Função: Cálculo de subconjuntos
+
+def subConj(n):
+    return subConj_recursivo([], sorted(n))
+
+
+def subConj_recursivo(atual, conj):
+    if conj:
+        return subConj_recursivo(atual, conj[1:]) + subConj_recursivo(atual + [conj[0]], conj[1:])
+    return [atual]
+
+
+# Entrada de Dados
+n = [1, 2, 3]
+resultado = subConj(n)
+
+print('O número de elementos do conjunto {} é {}'.format(n, len(resultado)))
+print('As partes do conjunto {} são: {}'.format(n, resultado))
+
+
