6117b3ea480cbe6b201a402b0ce314f35113db9a
RaizaCirne <raizabraz93@gmail.com>
2023-07-09 16:44:06 -0300

    feat: add novo programa

---
+++ Formação_Front-end/04.Curso_JavaScript/projeto_atual/4-conversaoDetipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2"); // concatenação
+console.log(parseInt("2") + parseInt("2")); // converte string para número
+
+console.log("7" / "2"); // no caso de divisão não precisamos usar o parseInt
+console.log("Ricardo" / "2"); // NaN - Not a Number
+
+console.log(6.5); // ponto flutuante para trabalhar com números
+console.log(6, 5); // não utilizar vírgula
