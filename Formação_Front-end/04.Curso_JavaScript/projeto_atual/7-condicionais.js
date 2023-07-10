5871423c3f69a7918ae019c74eb3702ff8fdfde6
RaizaCirne <raizabraz93@gmail.com>
2023-07-10 19:22:17 -0300

    feat: add programa

---
+++ Formação_Front-end/04.Curso_JavaScript/projeto_atual/7-condicionais.js
@@ -0,0 +1,44 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true; 
+
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+// // if e else controlam o fluxo de execução do código
+// if (idadeComprador >= 18) {
+//   console.log("Comprador maior de idade");
+//   listaDeDestinos.splice(1, 1); // removendo item
+// } else if (estaAcompanhada) {
+//   console.log("Comprador está acompanhado");
+//   listaDeDestinos.splice(1, 1); // removendo item
+// } else {
+//   console.log("Não é maior de Idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+  console.log("Boa Viagem!");
+  listaDeDestinos.splice(2, 1); // removendo item
+} else {
+  console.log("Não é maior de Idade e não posso vender");
+}
+
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada) {
+  console.log("Boa Viagem!")
+} else {
+  console.log("Você não pode embarcar")
+}
+
+console.log(listaDeDestinos);
+
+// Operadores lógicos
+// console.log(idadeComprador > 18); // maior que
+// console.log(idadeComprador < 18); // menor que
+// console.log(idadeComprador <= 18); // menor que ou igual
+// console.log(idadeComprador >= 18); // maior que ou igual
+// console.log(idadeComprador == 18); // operador de igualdade
