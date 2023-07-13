905aca86017f4477508f7353d14bc6c824b85338
RaizaCirne <raizabraz93@gmail.com>
2023-07-13 09:28:44 -0300

    feat: add revisão DOM

---
+++ Formação_Front-end/05.Curso_JavaScript-na-Web_DOM/modulos_revisao/anotacoes/anotacoes-modulo_1/querySelector.js
@@ -0,0 +1,5 @@
+document.querySelector('.titulo').textContent = "Comprar macã" 
+//  Utilizamos querySelector para percorrer a árvore do DOM
+// Através da propriedade textContent conseguimos acesso ao texto do parágrafo, e por fim, trocamos o texto.
+// Alteramos de forma dinâmica o conteúdo da aplicação. Para isso, percorremos o DOM. 
+
