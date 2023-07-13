9e22fedbd23599ea4217a5c79c198ff0d533dd51
RaizaCirne <raizabraz93@gmail.com>
2023-07-13 10:05:19 -0300

    feat: atualizar revisão DOM

--- Formação_Front-end/05.Curso_JavaScript-na-Web_DOM/modulos_revisao/anotacoes/anotacoes-modulo_1/01-querySelector.js
+++ Formação_Front-end/05.Curso_JavaScript-na-Web_DOM/modulos_revisao/anotacoes/anotacoes-modulo_1/01-querySelector.js
@@ -1,5 +1,5 @@
-document.querySelector('.titulo').textContent = "Comprar macã" 
-//  Utilizamos querySelector para percorrer a árvore do DOM
+document.querySelector('.content').textContent = "Comprar maçã";
+//  Utilizamos o método querySelector para percorrer a árvore do DOM e encontrar o elemento (parágrafo) utilizando JavaScript
 // Através da propriedade textContent conseguimos acesso ao texto do parágrafo, e por fim, trocamos o texto.
 // Alteramos de forma dinâmica o conteúdo da aplicação. Para isso, percorremos o DOM. 
 
