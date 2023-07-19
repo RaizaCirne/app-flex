6dff6b0c2235d35595b4534ba39d17c4d7b8cef7
RaizaCirne <raizabraz93@gmail.com>
2023-07-19 12:20:19 -0300

    feat: add preventDefault

--- Formação_Front-end/05.Curso_JavaScript-na-Web_DOM/revisao_modulos/revisao-modulo_2/listaDeTarefas_5.js
+++ Formação_Front-end/05.Curso_JavaScript-na-Web_DOM/revisao_modulos/revisao-modulo_2/listaDeTarefas_5.js
@@ -4,8 +4,7 @@ const criarTarefa = (evento) => {
 
   const input = document.querySelector('[data-form-input]'); 
   const valor = input.value; 
-  console.log(valor)
-
+  console.log(valor);
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
@@ -28,3 +27,33 @@ novaTarefa.addEventListener('click', criarTarefa);
 // Vamos chamar esse objeto de evento
 // Ele possui um método chamado preventDefault(), que serve para prevenir o comportamento padrão ->   evento.preventDefault();
 // Assim, conseguimos pegar a informação e não vai ter o refresh da página, não recarregou. Isso quer dizer que previnimos o comportamento de enviar o dado para algum lugar. Forçamos o formulário a fazer o que estavamos querendo. 
+
+// Organizamos melhor o código: 
+// Ele estava assim: 
+/*const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', (evento) => {
+
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]'); 
+  const valor = input.value; 
+
+  console.log(valor); 
+  input.value = " "
+}); */ 
+
+// 1º Criamos uma função -> const criarTarefa 
+// 2º Colocamos o input dentro dela ->   const input = document.querySelector('[data-form-input]'); 
+// 3º Colocamos o valor dentro dela -> const valor = input.value; 
+// 4º Colocamos o console.log dentro dela ->  console.log(valor);
+// 5º Passamos o preventDefault lá pra cima da função -> evento.preventDefault();
+// 6º Passamos o parâmetro para a função, então ela vai receber esse evento -> const criarTarefa = (evento) => { } 
+
+// No evento teremos o 'click', mas ao invés de ter uma função anônima executando o que a gente quer, vamos passar a função criarTarefa como referência, sem executar -> novaTarefa.addEventListener('click', criarTarefa)
+
+// Quer dizer que quando acionarmos o evento de 'click', criarTarefa vai chamar a função const criarTarefa = (evento) => { } e ele vai executar todas as instruções dentro dela 
+
+
+
+
+
