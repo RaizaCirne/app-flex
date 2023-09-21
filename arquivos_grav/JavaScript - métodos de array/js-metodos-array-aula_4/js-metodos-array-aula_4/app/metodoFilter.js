24c2073fda2d140b8c532214c0e22849a2465bde
Raiza Cirne <raizacirne@gmail.com>
2023-09-21 11:48:14 -0300

    feat: projeto js

---
+++ arquivos_grav/JavaScript - métodos de array/js-metodos-array-aula_4/js-metodos-array-aula_4/app/metodoFilter.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+}
