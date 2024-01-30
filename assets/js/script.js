c188f49ce67096af3cdd3b0f4671d00f695bfa55
Raiza Cirne <raizacirne@gmail.com>
2024-01-30 12:14:45 -0300

    Feat: add background

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ function calcular() {
   // Pegando valor do input e armazenando nas variáveis
   etanol = parseFloat(frmFlex.valorEtanol.value.replace(",","."));
   gasolina = parseFloat(frmFlex.valorGasolina.value.replace(",","."));
+  // condição 
+  // se o valor do etanol custar até 70% o valor da gasolina, etanol, senão, gasolina.
   if (etanol < 0.7 * gasolina) {
     document.getElementById("imagens").src="assets/images/etanol.png";
   } else {
@@ -11,6 +13,7 @@ function calcular() {
   }
 }
 
+// voltando a imagem para neutro
 function resetar() {
   document.getElementById("imagens").src="assets/images/neutro.png";
 }
